910915d7fb94c432f11297c96f8be8c84647a4d1
João Vitor Witt <joaovitorwitt2004@gmail.com>
2023-10-04 10:26:23 -0300

    preparing to deploy frontend

--- backend/api/settings.py
+++ backend/api/settings.py
@@ -18,6 +18,11 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 import os
 import time
 from datetime import timedelta
+import environ
+
+# environment variables settings
+env = environ.Env()
+environ.Env.read_env()
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
